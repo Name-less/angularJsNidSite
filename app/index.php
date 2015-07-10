14e5491cc67cc7283421a2ea6ecd043faf144f22
Name-less <achille@lenid.(none)>
2015-07-10 21:09:27 +0000

    change tab website name

--- app/index.php
+++ app/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>My AngularJS App</title>
+  <title>Le Nid</title>
     <link rel="stylesheet" href="app.css">
 
   <link rel="stylesheet" href="bower_components/html5-boilerplate/dist/css/normalize.css">
