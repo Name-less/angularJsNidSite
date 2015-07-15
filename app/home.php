3cdecdc9c40a8f67eb102bba42dfbf616de3613e
Name-less <achille@lenid.(none)>
2015-07-15 20:03:42 +0000

    css modified for home box which are now one at the right of the other

--- app/home.php
+++ app/home.php
@@ -6,31 +6,36 @@
 </head>
 <body>
 
-<div style="width:20%;margin-top:3%;margin-left:3%">
-<md-whiteframe class="md-whiteframe-z1" layout layout-align="center center">
+<div class="box">
+	<md-whiteframe class="md-whiteframe-z1" layout layout-align="center center">
 
 
-<div>
-        <md-input-container>
-          <label>Find your city</label>
-          <input ng-model="city_find">
-        </md-input-container>
-        <md-button ng-click="get_weather(city_find)">Get it !</md-button>
-	</br>
-		CITY : {{city}}
-		</br>
-		TEMERATURE : {{temp}} °C
-		</br>
-		MIN : {{min_temp}} °C
-		</br>
-		MAX : {{max_temp}} °C
-		</br>
-		WIND : {{wind_speed}} m/s
-		</br>
-		SKY : {{weather_type}}
-		</br>
+		<div>
+        		<md-input-container>
+          			<label>Find your city</label>
+          			<input ng-model="city_find">
+        		</md-input-container>
+        		<md-button ng-click="get_weather(city_find)">Get it !</md-button>
+			</br>
+			CITY : {{city}}
+			</br>
+			TEMERATURE : {{temp}} °C
+			</br>
+			MIN : {{min_temp}} °C
+			</br>
+			MAX : {{max_temp}} °C
+			</br>
+			WIND : {{wind_speed}} m/s
+			</br>
+			SKY : {{weather_type}}
+			</br>
+		</div>
+	</md-whiteframe>
 </div>
-</md-whiteframe>
+
+<div class="box">
+kvjdfpjvopdfjvpjfpdj
 </div>
-</head>
-<body>
+
+</body>
+</html>
